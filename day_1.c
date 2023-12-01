416de511b5718e34483bf96232e00f414f8aa356
Arkadius Motyl <arkamot@web.de>
2023-12-01 20:37:14 +0100

    Day 1, First Star Code works for my Input

--- day_1.c
+++ day_1.c
@@ -9,26 +9,58 @@ size_t get_single_calibration_value(char* line, size_t len) {
   uint16_t first_digit = 0;
   uint16_t last_digit = 0;
 
-  bool first = true;
+  bool first_digit_set = false;
 
   for (size_t idx = 0; idx < len; ++idx) {
     if (isdigit(line[idx])) {
-      if (first) {
-        first_digit = atoi(&line[idx]);
-        first = false;
+      // atoi() converts until it finds a null char, but since we're passing a whole string,
+      // we have to construct a temporary string with just the char and the terminator
+      char tmp[2] = {line[idx], '\0'};
+      uint16_t digit = atoi(tmp);
+
+      if (!first_digit_set) {
+        first_digit = digit;
+        last_digit = digit;
+        first_digit_set = true;
       } else {
-        last_digit = atoi(&line[idx]);
+        last_digit = digit;
       }
     }
   }
 
+  printf("first digit = %d, last_digit = %d\n", first_digit, last_digit);
+
   return first_digit * 10 + last_digit;
 }
 
-int main(int argc, char** argv) {
-  char* line = "1ha2llo3";
+int main() {
+  // Each line will be read into this buffer
+  size_t buffer_size = 512;
+  char buffer[buffer_size];
 
-  size_t value = get_single_calibration_value(line, strlen(line));
+  FILE* fp = NULL;
 
-  printf("Value %ld\n", value);
+  // Open the file containing the calibration values
+  fp = fopen("calibration.txt", "r");
+
+  if (fp == NULL){
+    exit(EXIT_FAILURE);
+  }
+
+  size_t calibration_value = 0;
+
+  size_t line_no = 1;
+
+  // Extract from each line the calibration value
+  while(fgets(buffer, buffer_size, fp)){
+    calibration_value += get_single_calibration_value(buffer, strlen(buffer));
+    printf("%ld: temp val = %ld\n", line_no, calibration_value);
+    line_no++;
+  }
+
+  printf("Calibration value = %ld\n", calibration_value);
+  
+  fclose(fp);
+
+  exit(EXIT_SUCCESS);
 }
